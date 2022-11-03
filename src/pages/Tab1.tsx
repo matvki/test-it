768bd825649f6e3616582a72b4c2844d6b3281c3
matvki <m.vecchi@it-students.fr>
2022-11-03 11:04:35 +0100

    v1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -5,19 +5,29 @@ import './Tab1.css';
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+          <IonHeader>
+                <IonToolbar>
+                      <IonTitle>Appli IT</IonTitle>
+                </IonToolbar>
+          </IonHeader>
+          <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 1</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <div id="container" className="mx-auto">
+                    <h1 className="ion-text-center">
+                        Choisissez votre bouton
+                    </h1>
+                    <div className="d-flex justify-content-center align-items-center row">
+                        <button className="col my-auto">Bouton 1</button>
+                        <button className="col my-auto">Bouton 2</button>
+                        <button className="col my-auto">Bouton 3</button>
+                        <button className="col my-auto">Bouton 4</button>
+                    </div>
+                </div>
+          </IonContent>
     </IonPage>
   );
 };
