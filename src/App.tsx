e2e61c1bf53c1b9cbf35ba272cf3cf9ec080fc55
matvki <m.vecchi@it-students.fr>
2022-11-03 16:10:37 +0100

    v3 - add button gyroscope

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -35,9 +36,33 @@ import './theme/variables.css';
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <Tab1 />
-  </IonApp>
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/tab1">
+              <Tab1 />
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon icon={triangle} />
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon icon={ellipse} />
+              <IonLabel>Tab 2</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import { Camera, CameraResultType } from '@capacitor/camera';
 import './Tab1.css';
+import {IonReactRouter} from "@ionic/react-router";
 
 const capture = async () => {
     const image = await Camera.getPhoto({
@@ -34,7 +34,13 @@ const Tab1: React.FC = () => {
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => navigator.vibrate(5000)}><img src="assets/icon/vibration.webp" alt="volume icon" height="90%" width="40%"/></button>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3').play()}><img src="assets/icon/volume.png" alt="volume icon" height="90%" width="60%"/></button>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={capture}><img src="assets/icon/picture.png" alt="volume icon" height="90%" width="50%"/></button>
-                        <button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/mystery.png" alt="volume icon" height="90%" width="60%"/></button>
+
+                        {/*<button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/mystery.png" alt="volume icon" height="90%" width="60%"/></button>*/}
+
+                        {/* do a button that redirect to Tab2*/}
+                        <IonReactRouter>
+                            <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => window.location.href = "/tab2"}><img src="assets/icon/gyroscope.png" alt="volume icon" height="90%" width="60%"/></button>
+                        </IonReactRouter>
                     </div>
                 </div>
           </IonContent>
