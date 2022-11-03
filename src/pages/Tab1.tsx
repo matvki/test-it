a3ca6319feb02068c8b5b903464af133c11c4700
Theo Parent <theoparent71@gmail.com>
2022-11-03 15:40:35 +0100

    Made camera, vibration and sound on the app

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,16 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { Camera, CameraResultType } from '@capacitor/camera';
 import './Tab1.css';
+
+const capture = async () => {
+    const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri
+    });
+  alert("Image sauvegardé dans votre galerie. Aucun probkème avec le plugin Camera.");
+}
 
 const Tab1: React.FC = () => {
   return (
@@ -21,9 +31,9 @@ const Tab1: React.FC = () => {
                         Choisissez votre bouton
                     </h1>
                     <div className="d-flex justify-content-center align-items-center row">
-                        <button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/vibration.webp" alt="volume icon" height="90%" width="40%"/></button>
-                        <button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/volume.png" alt="volume icon" height="90%" width="60%"/></button>
-                        <button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/picture.png" alt="volume icon" height="90%" width="50%"/></button>
+                        <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => navigator.vibrate(5000)}><img src="assets/icon/vibration.webp" alt="volume icon" height="90%" width="40%"/></button>
+                        <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3').play()}><img src="assets/icon/volume.png" alt="volume icon" height="90%" width="60%"/></button>
+                        <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={capture}><img src="assets/icon/picture.png" alt="volume icon" height="90%" width="50%"/></button>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/mystery.png" alt="volume icon" height="90%" width="60%"/></button>
                     </div>
                 </div>
