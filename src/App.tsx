8b3d4183449a100b4efbe18cb10de2948797be27
matvki <m.vecchi@it-students.fr>
2022-11-04 11:42:14 +0100

    update page 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -43,21 +42,14 @@ const App: React.FC = () => (
             <Route exact path="/tab1">
               <Tab1 />
             </Route>
-            <Route exact path="/tab2">
-              <Tab2 />
-            </Route>
             <Route exact path="/">
               <Redirect to="/tab1" />
             </Route>
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
-            <IonTabButton tab="tab1" href="/tab1">
+            <IonTabButton className="nav-button" tab="tab1" href="/tab1">
               <IonIcon icon={triangle} />
               <IonLabel>Tab 1</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon icon={ellipse} />
-              <IonLabel>Tab 2</IonLabel>
             </IonTabButton>
           </IonTabBar>
         </IonTabs>
