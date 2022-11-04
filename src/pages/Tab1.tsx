b0d33bc5584eafd82684996869bedfd4164f79ae
Theo Parent <theoparent71@gmail.com>
2022-11-04 09:06:23 +0100

    Maybe fixed rendering on phone

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -30,18 +30,13 @@ const Tab1: React.FC = () => {
                     <h1 className="ion-text-center">
                         Choisissez votre bouton
                     </h1>
-                    <div className="d-flex justify-content-center align-items-center row">
+
+                    <ion-row>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => navigator.vibrate(5000)}><img src="assets/icon/vibration.webp" alt="volume icon" height="90%" width="40%"/></button>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3').play()}><img src="assets/icon/volume.png" alt="volume icon" height="90%" width="60%"/></button>
                         <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={capture}><img src="assets/icon/picture.png" alt="volume icon" height="90%" width="50%"/></button>
-
-                        {/*<button className="col my-auto btn d-flex align-items-center justify-content-center"><img src="assets/icon/mystery.png" alt="volume icon" height="90%" width="60%"/></button>*/}
-
                         {/* do a button that redirect to Tab2*/}
-                        <IonReactRouter>
-                            <button className="col my-auto btn d-flex align-items-center justify-content-center" onClick={() => window.location.href = "/tab2"}><img src="assets/icon/gyroscope.png" alt="volume icon" height="90%" width="60%"/></button>
-                        </IonReactRouter>
-                    </div>
+                    </ion-row>
                 </div>
           </IonContent>
     </IonPage>
